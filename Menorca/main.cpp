61d8b9d6b70de3860babb6a792622f1c7bdb022b
Hapeing <hampushallstrom@hotmail.com>
2016-07-18 16:45:55 +0200

    installed SFML

--- Menorca/main.cpp
+++ Menorca/main.cpp
@@ -1,7 +1,34 @@
-#include <iostream>
+#include <SFML/Graphics.hpp>
+
 
 int main()
 {
-	std::cout << "hello world" << std::endl;
+	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+	sf::RenderWindow window(sf::VideoMode(600, 400), "Hello world");
+
+	sf::Clock clock;
+	float dt = 0;
+
+
+	while (window.isOpen())
+	{
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
+
+
+		dt = clock.restart().asSeconds();
+
+
+
+
+
+		window.clear();
+		window.display();
+	}
+
 	return 0;
 }
